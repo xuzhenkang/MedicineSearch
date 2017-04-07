28aa65b30b0fe6064eacb27fbf07c04629672c4d
xuzhenkang <xuzhenkang@hotmail.com>
2017-04-08 00:21:47 +0800

    优化，等待需求

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -1,12 +1,18 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -37,8 +43,7 @@ public class MainFrame extends JFrame {
 	// 表头
 	private String[] leftColumnNames = { "关键字", "文献数" }; // 左侧面板中的左侧面板中表格的表头
 	private String[] mainColumnNames = { "中医文献名", "作者", "朝代", "年份" }; // 左侧面板中的右侧面板中表格的表头
-	
-	
+
 	public MainFrame() {
 		// 初始化组件
 		this.initComponents();
@@ -48,27 +53,24 @@ public class MainFrame extends JFrame {
 		this.initLeftPanel();
 		// 右侧Panel
 		this.initRightPanel();
-		// 设定JFrame的总体布局
-		this.setLayout(new GridLayout(1, 2));
-		// 向JFrame面板中添加面板组件
-		this.add(leftPanel);
-		this.add(rightPanel);
 		// 设定JFrame属性
 		this.setFrame();
 	}
 
+	// 右侧Panel
 	private void initRightPanel() {
 		rightNorthPanel.add(new JLabel(""));
 		rightNorthPanel.add(rightLabel);
 		rightNorthPanel.add(searchNextButton);
-
+		searchNextButton.addActionListener(new MyButtonActionListener());
 		rightPanel.add(rightNorthPanel, BorderLayout.NORTH);
 		jTextArea.setEditable(false); // 设置不可编辑
-		jTextArea.setText("asdfasdfasdf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\nf\n\nadsf\nasdf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\ndf\n");
+		jTextArea.setText("Read Only o asdf ");
 		rightScrollPane = new JScrollPane(jTextArea);
 		rightPanel.add(rightScrollPane, BorderLayout.CENTER);
 	}
 
+	// 左侧Panel
 	private void initLeftPanel() {
 		// 左侧Panel
 		leftLabelPanel.add(leftLabel);
@@ -80,39 +82,45 @@ public class MainFrame extends JFrame {
 				return false;
 			}
 		};
+		leftTable.setName("LeftTable");
 		leftTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);// 仅可以单选
-		
+		leftTable.setRowSelectionInterval(0, 0);
+		leftTable.addMouseListener(new MyMouseOnTableAdapter(leftTable));
 		lLeftScrollPane = new JScrollPane(leftTable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		lleftPanel.add(lLeftScrollPane, BorderLayout.CENTER);
 		lleftPanel.setPreferredSize(new Dimension(130, 500));
-		
+
 		// 中间Panel
 		mainLabelPanel.add(mainLabel);
 		lMainPanel.add(mainLabelPanel, BorderLayout.NORTH);
-		mainTable = new JTable(mainData, mainColumnNames){
+		mainTable = new JTable(mainData, mainColumnNames) {
 			private static final long serialVersionUID = 1L;
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				return false;
 			}
 		};
+		mainTable.setName("MainTable");
 		mainTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // 仅可以单选
+		mainTable.setRowSelectionInterval(0, 0);
+		mainTable.addMouseListener(new MyMouseOnTableAdapter(mainTable));
 		lMainScrollPane = new JScrollPane(mainTable);
 		lMainPanel.add(lMainScrollPane, BorderLayout.CENTER);
 		lMainPanel.setPreferredSize(new Dimension(267, 500));
-		
+
 		// 设定布局
 		leftPanel.add(lleftPanel, BorderLayout.WEST);
 		leftPanel.add(lMainPanel, BorderLayout.EAST);
 	}
 
+	// 初始化组件
 	private void initComponents() {
 		leftPanel = new JPanel(new BorderLayout());
 		lMainPanel = new JPanel(new BorderLayout());
 		lleftPanel = new JPanel(new BorderLayout());
 		rightPanel = new JPanel(new BorderLayout());
-		rightNorthPanel = new JPanel(new GridLayout(1,3));
+		rightNorthPanel = new JPanel(new GridLayout(1, 3));
 		jTextArea = new JTextArea();
 		leftLabel = new JLabel("相关关键词");
 		mainLabel = new JLabel("中医文献");
@@ -122,6 +130,7 @@ public class MainFrame extends JFrame {
 		mainLabelPanel = new JPanel();
 	}
 
+	// 加载数据 要写到模型层或控制层
 	private void loadData() {
 		// 创建显示数据
 		leftData = new Object[50][2];
@@ -131,19 +140,62 @@ public class MainFrame extends JFrame {
 		}
 		mainData = new Object[50][4];
 		for (int i = 0; i < mainData.length; i++) {
-			mainData[i][0] = "本草纲目";
+			mainData[i][0] = "本草纲目本草纲目本草纲目本草纲目本草纲目本草纲目";
 			mainData[i][1] = "李时珍";
 			mainData[i][2] = "明";
 			mainData[i][3] = "1518年";
 		}
 	}
 
+	// 设定Frame面板
 	private void setFrame() {
+		// 设定JFrame的总体布局
+		this.setLayout(new GridLayout(1, 2));
+		// 向JFrame面板中添加面板组件
+		this.add(leftPanel);
+		this.add(rightPanel);
 		this.setTitle("基础检测子系统");
 		this.setSize(800, 600);
 		this.setResizable(false);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+	}
+
+	class MyMouseOnTableAdapter extends MouseAdapter {
+		private JTable jTable;
+
+		public MyMouseOnTableAdapter(JTable jTable) {
+			this.jTable = jTable;
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			int selectedRow = jTable.getSelectedRow();
+			String message = jTable.getName() + " pressed " + selectedRow + " row selected!";
+			System.out.println(message);
+			jTextArea.setText(message + "中文测试");
+		}
+	}
+
+	class MyButtonActionListener implements ActionListener {
+		private JTable jTable;
+		public MyButtonActionListener(JTable jTable) {
+			this.jTable = jTable;
+		}
+		public MyButtonActionListener(){}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			/*没有用这句*/
+			if (this.jTable != null)
+				jTable.setRowSelectionInterval(0, jTable.getSelectedRow() + 1);
+			
+			jTextArea.select(0, 5);
+			jTextArea.requestFocus();
+			
+			// 判断是否有下一个，如果没有打印给出下面的提示框
+			//JOptionPane.showMessageDialog(MainFrame.this, "没有找到下一个", "提示", JOptionPane.CLOSED_OPTION);
+		}
+
 	}
 }
